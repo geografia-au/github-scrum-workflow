0f23893d8ad494a787929710a1fe521fd5ab438e
Shoaib Burq <saburq@gmail.com>
2017-06-30 07:57:21 +1000

    fix pagination

--- prepare-repository.rb
+++ prepare-repository.rb
@@ -4,10 +4,11 @@ require "chroma"
 
 repo = ARGV[0]
 
-client = Octokit::Client.new(:access_token => YAML.load_file('config/credentials.yml')['access_token'])
+client = Octokit::Client.new(access_token: YAML.load_file('config/credentials.yml')['access_token'])
+
 puts "Great! logged in as #{client.user.login}"
 
-existing_labels = client.labels(repo)
+existing_labels = client.labels(repo, per_page: 50).map(&:name)
 
 scrum = YAML.load_file('config/scrum.yml')
 
@@ -15,7 +16,7 @@ color = scrum['priorities']['color'].paint
 scrum['priorities']['labels'].each_with_index do |label, i|
   unless existing_labels.include?(label)
     puts "adding #{label} to #{repo}"
-    client.add_label repo,  label, color.darken(i-1*20).to_hex.sub('#','')    
+    client.add_label( repo, label, color.darken(i-1*20).to_hex.sub('#','') )
   end
 end
 
@@ -23,7 +24,7 @@ color = scrum['points']['color'].paint
 scrum['points']['labels'].each_with_index do |label, i|
   unless existing_labels.include?(label)
     puts "adding #{label} to #{repo}"
-    client.add_label repo,  label, color.darken(i-1*20).to_hex.sub('#','')    
+    client.add_label( repo, label, color.darken(i-1*20).to_hex.sub('#','') )
   end
 end
 
@@ -31,7 +32,7 @@ color = scrum['status']['color'].paint
 scrum['status']['labels'].each_with_index do |label, i|
   unless existing_labels.include?(label)
     puts "adding #{label} to #{repo}"
-    client.add_label repo,  label, color.darken(i-1*20).to_hex.sub('#','')    
+    client.add_label( repo, label, color.darken(i-1*20).to_hex.sub('#','') )
   end
 end
 
@@ -40,11 +41,11 @@ colors = colors+colors[2].paint.palette.tetrad
 scrum['types']['labels'].each_with_index do |label, i|
   unless existing_labels.include?(label)
     puts "adding #{label} to #{repo}"
-    client.add_label repo, label, colors[i].to_hex.sub('#','')
+    client.add_label( repo, label, colors[i].to_hex.sub('#','') )
   end  
 end
 
 # delete old labels
 ["bug", "duplicate", "enhancement", "help wanted", "invalid", "wontfix", "question"].each do |label|
-  client.delete_label!(repo, label) if labels.include?(label)
+  client.delete_label!(repo, label) if existing_labels.include?(label)
 end
